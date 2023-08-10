9ad9803ed61eb0fcbcc3f46a63206dda448c6fd7
gajjarmeshw <gajjarmeshw@gmail.com>
2023-08-10 14:54:30 +0530

    vercel

--- expenseswebsite/build_files.sh
+++ expenseswebsite/build_files.sh
@@ -1,4 +1,8 @@
 echo " BUILD START"
-python3.10 -m pip install -r requirements.txt
-python3.10 manage.py collectstatic --noinput --clear
-echo " BUILD END" 
+python3.9 -m pip install -r requirements.txt
+
+echp "Make Migrations"
+python 3.9 manage.py makemigrations --noinput
+python 3.9 manage.py migrate
+python3.9 manage.py collectstatic --noinput --clear
+echo " BUILD END"  
